da80c659d7ea1b3b33a65600c8e6a1991eecfea3
Javier Acevedo <javier.acevedohdz@hotmail.com>
2024-02-21 22:26:58 -0600

    Añade código inicial de arreglo

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@ namespace aplicacionListas
 {
     public partial class Form1 : Form
     {
+        private List<string> elementos = new List<string>();
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +10,24 @@ namespace aplicacionListas
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        private void AgregarButton_Click(object sender, EventArgs e)
+        {
+            string nuevoElemento = elementoTextBox.Text;
+            elementos.Add(nuevoElemento);
+            ActualizarListBox();
+            elementoTextBox.Clear();
+        }
+
+        private void ActualizarListBox()
+        {
+            elementosListBox.Items.Clear();
+            foreach (string elemento in elementos)
+            {
+                elementosListBox.Items.Add(elemento);
+            }
         }
     }
 }
